2be88eb00db6e387a60f096cd5ff61729cda3e5b
Johan Sebastian Herrera Melgarejo <102569088+Cvaslml@users.noreply.github.com>
2022-06-06 08:47:40 -0500

    Add files via upload

---
+++ for12.py
@@ -0,0 +1,17 @@
+"""Generar la siguiente serie: 1, 1/2, 1/3, 1/4, 1/5, 1/6, ... n"""
+
+#Input
+n = int(input("Digite el valor de n:"))
+#Processing
+s = "Serie: ("
+for i in range(1, n+1):
+    t = 1/i
+    # t = i*i
+    if i < n:
+        s = s + str(t) + ", "
+    else:
+        s = s + str(t) + ")"
+
+#Output
+print("--- Resultados ---")
+print(s)
