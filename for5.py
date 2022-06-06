14836c85391a9adc2d50984f511d9c63519d12f1
Johan Sebastian Herrera Melgarejo <102569088+Cvaslml@users.noreply.github.com>
2022-06-06 07:20:55 -0500

    Add files via upload

--- for5.py
+++ for5.py
@@ -1,28 +1,22 @@
-"""Programa para digitar el nombre y cuente cuantas vocales hay en el nombre. Con la funcion for"""
+"""Hacer el diagrama de flujo y el programa en Python que genere 1000 numeros aleatorios y que averigue e imprima cuantos son pares y cuantos son impares"""
 
-print("------------------------------------")
-print("--------Contador de vocales---------")
-print("------------------------------------")
+print("--------------------------------------------")
+print("--------------PARES E IMPARES---------------")
+print("--------------------------------------------")
+import random
+# input
+pares = 0
+impares = 0
 
-#Input
-n = input("Digite su nombre: ")
-contador_a = 0
-contador_e = 0
-contador_i = 0
-contador_o = 0
-contador_u = 0
+#Processing
+for i in range(1000):
+    num_aleatorio = random.randit(1,200)
+    if num_aleatorio%2 == 0:
+        pares += 1
+    else:
+        impares += 1
 
-for vocal in n:
-    if vocal.lower() in "a":
-        contador_a += 1
-    elif vocal.lower() in "e":
-        contador_e += 1
-    elif vocal.lower() in "i":
-        contador_i += 1
-    elif vocal.lower() in "o":
-        contador_o += 1
-    elif vocal.lower() in "u":
-        contador_u += 1
-print("El nombre " + str(n) + " contiene " + str(contador_a) + " vocales a, " + str(contador_e) + " vocales e, " + str(contador_i) + " vocales i, " + str(contador_o) + " vocales o, " + str(contador_u) + " vocales u ")
-
-    
+#Output
+print("Resultados: ")
+print(f"Cantidad de pares = {pares}")
+print(f"Cantidad de impares = {impares}")
